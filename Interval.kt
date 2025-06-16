cc99bbc8c18a0ea6aed2715ebc6bdc3c04d266d2
Neloy007 <neloymac24@gmail.com>
2025-06-16 16:51:26 +0600

    using if-else

--- Interval.kt
+++ Interval.kt
@@ -3,12 +3,20 @@ import java.util.*
 fun main(args: Array<String>) {
 
     val num = readLine()!!.toDouble()
-    when {
-        num<0||num>100 -> println("Fora de intervalo")
-        num>=0 && num<=25 ->  println("Intervalo [0,25]")
-        num>25 && num<=50 -> println("Intervalo (25,50]")
-        num>50 && num<=75 -> println("Intervalo (50,75]")
-        num>75 && num<=100 -> println("Intervalo (75,100]")
+    if (num<0||num>100){
+        println("Fora de intervalo")
+    }
+    else if (num>=0 && num<=25) {
+        println("Intervalo [0,25]")
+    }
+    else if (num>25 && num<=50) {
+        println("Intervalo (25,50]")
+    }
+    else if (num>50 && num<=75) {
+        println("Intervalo (50,75]")
+    }
+    else if (num>75 && num<=100) {
+        println("Intervalo (75,100]")
     }
 
 }
