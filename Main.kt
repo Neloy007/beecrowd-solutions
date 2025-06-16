32a4ac084fcebede8d043eda748b796d1e9bc64a
Neloy007 <neloymac24@gmail.com>
2025-06-16 16:43:02 +0600

    init commit

--- Main.kt
+++ Main.kt
@@ -3,21 +3,23 @@ fun main(args: Array<String>) {
     val (a, b, c) = readLine()!!.split(" ").map { it.toDouble() }
     val discriminant = (b.pow(2))-(4*a*c)
     if (a == 0.0){
-        println("It's Not a quadratic equation. It can be Linear.")
+        //It's Not a quadratic equation. It can be Linear.
+
     }else if (discriminant<0.0){
-        println("It has two complex roots.(Imaginary outputs)")
+        //It has two complex roots.(Imaginary outputs)
+
 
     }
     else if (discriminant==0.0){
-        //when discreminent = 0
-        println("It has two real and equal roots.(two real and equal roots)")
+        // It has two real and equal roots.(two real and equal roots)
+
         val x = -b/(2*a)
         println("Two real and Equal roots: $x")
 
     }
     else {
+        // It has two real and distinct roots.
 
-        println("It has two real and distinct roots.")
         val x1 = (-b+sqrt(discriminant))/(2*a)
         val x2 = (-b-sqrt(discriminant))/(2*a)
         println("Root1 = %.5f".format(x1))
