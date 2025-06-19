6ff145b54d719e0b904fc9d32dad7dc6f2b5976e
Neloy007 <neloymac24@gmail.com>
2025-06-20 00:21:58 +0600

    flexible ways to initialize a class

--- Constructor.kt
+++ Constructor.kt
@@ -1,15 +1,18 @@
 class Person(val name: String){
     val age:Int =0    //age is initialized by the secondary constructor, so it must be declared in the class body.
-    init {
-        println("My name is $name")
-    }
+
+
     constructor(name: String,age:Int):this(name){
         this.age
-        println("I'm $age years old.")
+
+        println("$name is $age years old.")
     }
 }
 
 fun main(args: Array<String>) {
-    val person = Person("Niloy",22)
+    val person1 = Person("Niloy",)
+    println("My name is ${person1.name}")
+    val parson2 = Person("Rubel",22)
+    val person3 = Person("Akash",23)
 
 }
