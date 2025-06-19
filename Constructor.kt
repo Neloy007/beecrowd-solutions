b4432529f235c096d4e74bf11850ffa37b12c99c
Neloy007 <neloymac24@gmail.com>
2025-06-20 00:11:04 +0600

    secondary constructor

--- Constructor.kt
+++ Constructor.kt
@@ -1,10 +1,15 @@
 class Person(val name: String){
+    val age:Int =0    //age is initialized by the secondary constructor, so it must be declared in the class body.
     init {
         println("My name is $name")
+    }
+    constructor(name: String,age:Int):this(name){
+        this.age
+        println("I'm $age years old.")
     }
 }
 
 fun main(args: Array<String>) {
-    val person = Person("Niloy")
+    val person = Person("Niloy",22)
 
 }
